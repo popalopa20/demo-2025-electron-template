15ad2649d71aaa2e99a1abd87301bd03c813aa03
root <root@LAPTOP-7J85VK73>
2025-04-16 14:53:33 +0300

    sql

--- src/main/db.js
+++ src/main/db.js
@@ -3,7 +3,7 @@ import { Client } from 'pg';
 export default async () => {
   const client = new Client({
     user: 'postgres',
-    password: 'postgres',
+    password: '2005',
     host: 'localhost',
     port: '5432',
     database: 'demo_2025',
